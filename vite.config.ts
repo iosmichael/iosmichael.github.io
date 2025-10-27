319586ae1bc342039c7ba695c40ccdaae4856c0e
iosmichael <xinyu_liu7@apple.com>
2025-10-26 18:42:39 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/iosmichael.github.io/' : '/',
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
